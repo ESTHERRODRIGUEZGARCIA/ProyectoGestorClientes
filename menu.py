da4853eb62dd24e3ed579506bb4b4e20e879bd29
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-06 22:08:44 +0200

    csv creado

--- menu.py
+++ menu.py
@@ -33,12 +33,13 @@ def iniciar():
             print(cliente) if cliente else print("Cliente no encontrado.")
         if opcion == '3':
             print("Añadiendo un cliente...\n")
-            dni = helpers.leer_texto(
-            3, 3, "DNI (2 ints y 1 char)").upper()
-            nombre = helpers.leer_texto(
-            2, 30, "Nombre (de 2 a 30 chars)").capitalize()
-            apellido = helpers.leer_texto(
-            2, 30, "Apellido (de 2 a 30 chars)").capitalize()
+            #Comprobación de DNI válido
+            while 1:
+                dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+                if helpers.dni_valido(dni, db.Clientes.lista):
+                    break
+            nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
+            apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
             db.Clientes.crear(dni, nombre, apellido)
             print("Cliente añadido correctamente.")
         if opcion == '4':
@@ -46,10 +47,8 @@ def iniciar():
             dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
             cliente = db.Clientes.buscar(dni)
             if cliente:
-                nombre = helpers.leer_texto(
-                2, 30, f"Nombre (de 2 a 30 chars) [{cliente.nombre}]").capitalize()
-                apellido = helpers.leer_texto(
-                2, 30, f"Apellido (de 2 a 30 chars) [{cliente.apellido}]").capitalize()
+                nombre = helpers.leer_texto(2, 30, f"Nombre (de 2 a 30 chars) [{cliente.nombre}]").capitalize()
+                apellido = helpers.leer_texto(2, 30, f"Apellido (de 2 a 30 chars) [{cliente.apellido}]").capitalize()
                 db.Clientes.modificar(cliente.dni, nombre, apellido)
                 print("Cliente modificado correctamente.")
             else:
