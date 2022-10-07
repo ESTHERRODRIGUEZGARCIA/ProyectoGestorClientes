07223a424ee537c2ef0ed9473a9e34b200ade155
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-07 10:52:24 +0200

    sitio equivocado

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
+import sys
+import gestor.ui as ui
 import gestor.menu as menu
+
 if __name__ == "__main__":
-    menu.iniciar()
+    if len(sys.argv) > 1 and sys.argv[1] == "-t":
+        menu.iniciar()
+    else:
+        app = ui.MainWindow()
+        app.mainloop()
